834ff7752fae8c9f0ea659f62a88a7780421a0cb
Tech Savvy <briannyaberi1@.gmail.com>
2023-01-30 10:51:59 +0300

    Server and client setup

---
+++ server/index.js
@@ -0,0 +1,11 @@
+requre('dotenv').config();
+const express = ('express');
+const app = express();
+const cors = require('cors');
+
+// mmiddlewares
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 8090;
+app.listen(port, () => console.log(`Listening on port ${port}`));

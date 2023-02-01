6f5494a12923133d00a0b183915aa1da6ed6e811
Tech Savvy <briannyaberi1@.gmail.com>
2023-02-01 11:23:30 +0300

    Done with the register Router

--- server/routes/users.js
+++ server/routes/users.js
@@ -13,8 +13,13 @@ router.post("/", async (req, res) => {
         if (user) {
             return res.status(409).send({message:error.details[0].message});
         }
+        const salt = await bcrypt.genSalt(Number(process.env.SALT));
+        const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+        await new User({...req.body, password: hashPassword}).save();
+        res.status(201).send({message: "User created successfully"});
     } catch (error) {
-        
+        res.status(500).send({message: "Internal server Error"});
     }
 });
 

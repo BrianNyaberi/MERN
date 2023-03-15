a130de50ae4e0da5da186aa7e897113c2c6124ef
Tech Savvy <briannyaberi1@.gmail.com>
2023-03-15 16:51:47 +0300

    Signed-off-by: Tech Savvy <briannyaberi1@.gmail.com>

--- client/src/components/Singup/index.jsx
+++ client/src/components/Singup/index.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styles from "./styles.module.css";
 
-const Login = () => {
-	const [data, setData] = useState({ email: "", password: "" });
+const Signup = () => {
+	const [data, setData] = useState({
+		firstName: "",
+		lastName: "",
+		email: "",
+		password: "",
+	});
 	const [error, setError] = useState("");
+	const navigate = useNavigate();
 
 	const handleChange = ({ currentTarget: input }) => {
 		setData({ ...data, [input.name]: input.value });
@@ -14,10 +20,10 @@ const Login = () => {
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		try {
-			const url = "http://localhost:8080/api/auth";
+			const url = "http://localhost:8080/api/users";
 			const { data: res } = await axios.post(url, data);
-			localStorage.setItem("token", res.data);
-			window.location = "/";
+			navigate("/login");
+			console.log(res.message);
 		} catch (error) {
 			if (
 				error.response &&
@@ -30,11 +36,37 @@ const Login = () => {
 	};
 
 	return (
-		<div className={styles.login_container}>
-			<div className={styles.login_form_container}>
+		<div className={styles.signup_container}>
+			<div className={styles.signup_form_container}>
 				<div className={styles.left}>
+					<h1>Welcome Back</h1>
+					<Link to="/login">
+						<button type="button" className={styles.white_btn}>
+							Sing in
+						</button>
+					</Link>
+				</div>
+				<div className={styles.right}>
 					<form className={styles.form_container} onSubmit={handleSubmit}>
-						<h1>Login to Your Account</h1>
+						<h1>Create Account</h1>
+						<input
+							type="text"
+							placeholder="First Name"
+							name="firstName"
+							onChange={handleChange}
+							value={data.firstName}
+							required
+							className={styles.input}
+						/>
+						<input
+							type="text"
+							placeholder="Last Name"
+							name="lastName"
+							onChange={handleChange}
+							value={data.lastName}
+							required
+							className={styles.input}
+						/>
 						<input
 							type="email"
 							placeholder="Email"
@@ -55,21 +87,13 @@ const Login = () => {
 						/>
 						{error && <div className={styles.error_msg}>{error}</div>}
 						<button type="submit" className={styles.green_btn}>
-							Sing In
-						</button>
-					</form>
-				</div>
-				<div className={styles.right}>
-					<h1>New Here ?</h1>
-					<Link to="/signup">
-						<button type="button" className={styles.white_btn}>
 							Sing Up
 						</button>
-					</Link>
+					</form>
 				</div>
 			</div>
 		</div>
 	);
 };
 
-export default Login;
+export default Signup;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
-requre('dotenv').config();
-const express = ('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
+const connection = require("./db");
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
 
-// mmiddlewares
+// database connection
+connection();
+
+// middlewares
 app.use(express.json());
 app.use(cors());
 
-const port = process.env.PORT || 8090;
-app.listen(port, () => console.log(`Listening on port ${port}`));
+// routes
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+
+const port = process.env.PORT || 8080;
+app.listen(port, console.log(`Listening on port ${port}...`));
